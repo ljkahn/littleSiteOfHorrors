2cd9246911ac96cc0950bd56cb8b614bbc1bdecf
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-10-05 14:36:59 -0500

    main handlebars done

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const path = require('path');
 const controllers = require("./controllers");
 const sequelize = require("./config/connection");
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(controllers);
 
