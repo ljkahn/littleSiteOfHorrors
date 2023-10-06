bb29fc1ee23c686a9ecad95b0a6ed9f5bc62e2ad
ljkahn <lia.j.kahn@gmail.com>
2023-10-06 12:12:36 -0600

    created movie-routes.js and added route to get all movie posters

---
+++ controllers/api/movie-routes.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const { Movie } = require('../../models');
+
+// /movies get all poster images
+// http://localhost:3001/movies/
+app.get('/movies', async (req, res) => {
+    try {
+      const posterData = await Movie.findAll({
+        // Selecting only the 'movie_url' attribute
+        attributes: ['poster_url'], 
+      });
+  
+      // Extracting movie URLs from the result
+      const posterUrl = posterData.map((movie) => movie.poster_url);
+  
+      res.json(posterUrl);
+    } catch (error) {
+      console.error('Error fetching movie URLs:', error);
+      res.status(500).json({ error: 'An error occurred while fetching movie URLs' });
+    }
+  });
+
+
+
+// /movies/:id generate title on page
+
+// /movies/:id generate director on page
+
+
+// /movies/:id generate year on page
+
+// /movies/:id generate summary on page
+
+// /movies/:id generate rating /spooky scale on page
+
+// /movies/:id create modal for user reviews to save to reviews
+
+// /movies/:id generate reviews from reviews table
+
+
+module.exports = router;
