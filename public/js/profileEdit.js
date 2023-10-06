c9954a4674764f6cf7034ffe5a1c7f16e900d864
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-10-06 16:39:22 -0500

    req.body returns all inputs and selections for profileEdit front end

--- public/js/profileEdit.js
+++ public/js/profileEdit.js
@@ -7,7 +7,7 @@ const saveProfileBtn = $('#save-profile-btn');
 
 const userProfileData = {}
 
-
+//Iterating through the array of svg's that users can choose as their profile image.
 let options = [];
 for (let i = 1; i < 21; i++) {
   options.push({
@@ -16,7 +16,7 @@ for (let i = 1; i < 21; i++) {
   });
   
 }
-
+//selecting the value of SVG 
 options.forEach( ({text, value}) => {
   const optionEl = $("<option>");
   optionEl.attr("value", value).text(text)
@@ -25,6 +25,8 @@ options.forEach( ({text, value}) => {
 
 
 //FUNCTIONS
+
+//Changing preview icon to selected user SVG
 function iconChange (event) {
   const userIconSelection = this.value
   userProfileData.user_icon = userIconSelection;
@@ -32,10 +34,35 @@ function iconChange (event) {
 
 }
 
+$( function() {
+  $( "#sortable" ).sortable();
+} );
+
+
 function submitChanges (event) {
   const name = $('#name');
+  const location = $('#location');
+  const answer1 = $('#answer_1');
+  const answer2 = $('#answer_2');
+  const answer3 = $('#answer_3');
+  const spookyScale = $('#spooky_scale');
   if (name.val()) {
     userProfileData.name = name.val();
+  } 
+  if (location.val()) {
+    userProfileData.location = location.val();
+  }
+  if (spookyScale.val()) {
+    userProfileData.spooky_scale = spookyScale.val();
+  }
+  if (answer1.val()) {
+    userProfileData.answer1 = answer1.val();
+  }
+  if (answer2.val()) {
+    userProfileData.answer2 = answer2.val();
+  }
+  if (answer3.val()) {
+    userProfileData.answer3 = answer3.val();
   }
   console.log(userProfileData);
   //GET THE REST OF THE VALUES TO SHOW UP IN REQ.BODY
