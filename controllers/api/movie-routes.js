d656d58b57cccd0b0adb847d439cee128afd4e6b
ljkahn <lia.j.kahn@gmail.com>
2023-10-06 14:02:46 -0600

    updated movie-routes.js for generating title, director, description, and year on the page

--- controllers/api/movie-routes.js
+++ controllers/api/movie-routes.js
@@ -3,7 +3,7 @@ const { Movie } = require('../../models');
 
 // /movies get all poster images
 // http://localhost:3001/movies/
-app.get('/movies', async (req, res) => {
+router.get('/movies', async (req, res) => {
     try {
       const posterData = await Movie.findAll({
         // Selecting only the 'movie_url' attribute
@@ -11,9 +11,9 @@ app.get('/movies', async (req, res) => {
       });
   
       // Extracting movie URLs from the result
-      const posterUrl = posterData.map((movie) => movie.poster_url);
+      const posterUrls = posterData.map((movie) => movie.poster_url);
   
-      res.render('all-posters', { posterUrl });
+      res.render('searchResults', { posterUrls });
     } catch (error) {
       console.error('Error fetching movie URLs:', error);
       res.status(500).json({ error: 'An error occurred while fetching movie URLs' });
@@ -23,13 +23,74 @@ app.get('/movies', async (req, res) => {
 
 
 // /movies/:id generate title on page
+router.get('/movie/:id', async (req, res) => {
+    try {
+      const movieId = parseInt(req.params.id);
+      const movie = await Movie.findByPk(movieId);
+  
+      if (!movie) {
+        res.status(404).send('Movie not found');
+      } else {
+        res.render('searchResults', { title: movie.title });
+      }
+    } catch (error) {
+      console.error('Error fetching movie title:', error);
+      res.status(500).send('An error occurred while fetching the movie title.');
+    }
+  });
+
 
 // /movies/:id generate director on page
+router.get('/movie/:id', async (req, res) => {
+    try {
+      const movieId = parseInt(req.params.id);
+      const movie = await Movie.findByPk(movieId);
+  
+      if (!movie) {
+        res.status(404).send('Movie not found');
+      } else {
+        res.render('searchResults', { title: movie.director });
+      }
+    } catch (error) {
+      console.error('Error fetching movie director:', error);
+      res.status(500).send('An error occurred while fetching the movie director.');
+    }
+  });
 
 
 // /movies/:id generate year on page
+router.get('/movie/:id', async (req, res) => {
+    try {
+      const movieId = parseInt(req.params.id);
+      const movie = await Movie.findByPk(movieId);
+  
+      if (!movie) {
+        res.status(404).send('Movie not found');
+      } else {
+        res.render('searchResults', { title: movie.release_year });
+      }
+    } catch (error) {
+      console.error('Error fetching release year:', error);
+      res.status(500).send('An error occurred while fetching the release year.');
+    }
+  });
 
 // /movies/:id generate summary on page
+router.get('/movie/:id', async (req, res) => {
+    try {
+      const movieId = parseInt(req.params.id);
+      const movie = await Movie.findByPk(movieId);
+  
+      if (!movie) {
+        res.status(404).send('Movie not found');
+      } else {
+        res.render('searchResults', { title: movie.description });
+      }
+    } catch (error) {
+      console.error('Error fetching movie year:', error);
+      res.status(500).send('An error occurred while fetching the movie year.');
+    }
+  });
 
 // /movies/:id generate rating /spooky scale on page
 
